1965f97256e35c22a0a16be9fb7e8e6622339ac7
Jackode <jkangliu@gmail.com>
2014-12-21 23:40:07 +0800

    commit add MaxStack

---
+++ src/main/java/jackode/algorithm/dataStructure/stack/MaxStack.java
@@ -0,0 +1,35 @@
+package jackode.algorithm.dataStructure.stack;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+/**
+ * design a stack, the complexity of get max value, push, pop is O(1)
+ * Created by liujiankang on 12/21/14.
+ */
+public class MaxStack<T extends Comparable> {
+    Stack<T> stack;
+    Stack<T> secondStack;
+
+    public MaxStack(){
+        stack = new Stack<T>();
+        secondStack = new Stack<T>();
+    }
+
+    public void push(T val){
+        stack.push(val);
+        if (secondStack.isEmpty() || val.compareTo(secondStack.peek()) > 0) secondStack.push(val);
+        else secondStack.push(secondStack.peek());
+    }
+
+    public T max(){
+        if (secondStack.isEmpty()) throw new EmptyStackException();
+        return secondStack.peek();
+    }
+
+    public T pop(){
+        if (secondStack.isEmpty() || stack.isEmpty()) throw new EmptyStackException();
+        secondStack.pop();
+        return stack.pop();
+    }
+}
